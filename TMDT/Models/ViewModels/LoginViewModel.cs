bc98bbb2b6a79004648543999e52a2eb0498c8f5
Võ Tấn Dũng <22dh110638@st.huflit.edu.vn>
2025-02-24 00:18:39 +0700

    TanDung

---
+++ TMDT/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_commerce.Models.ViewModel
+{
+	public class LoginViewModel
+	{
+		public int Id { get; set; }
+		[Required(ErrorMessage = "Please Type Your Username")]
+		public string UserName { get; set; }
+		[DataType(DataType.Password), Required(ErrorMessage = "Please Type Your Password")]
+		public string Password { get; set; }
+		public string ReturnUrl { get; set; }
+	}
+}
